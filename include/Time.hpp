55397ad3c331dc5366123d1e67b2fb324078795d
michalloska <loska.me@gmail.com>
2021-01-09 17:51:38 +0100

    Time implementation finished

--- include/Time.hpp
+++ include/Time.hpp
@@ -2,6 +2,24 @@
 #include <utility>
 #include <ostream>
 
-using Time = std::pair<int,int>;
+class Time
+{
+public:
+    Time(int hours, int minutes);
+    ~Time() = default;
+    int getHours() const;
+    int getMinutes() const;
+    Time operator+(const Time &r_time) const;
+    Time operator-(const Time &r_time) const;
+    bool operator==(const Time &r_time) const;
+    bool operator!=(const Time &r_time) const;
+    bool isTimeValid(const Time &time) const;
+    Time negate();
+private:
+    bool isHourAndMinutesValid(int hours, int minutes) const;
+    
+    int hours;
+    int minutes;
+};
 
-std::ostream& operator<<(std::ostream & out, const Time & time);
+std::ostream &operator<<(std::ostream &out, const Time &time);
--- src/Time.cpp
+++ src/Time.cpp
@@ -1,7 +1,110 @@
-#include "include/Time.hpp"
+#include "../include/Time.hpp"
+#include <cmath>
+#include <stdexcept>
+#include <string>
+#include <iostream>
+#include <iomanip>
 
-std::ostream& operator<<(std::ostream & out, const Time & time)
+Time::Time(int hours, int minutes)
 {
-    out << "Time: " << time.first << ":" << time.second;
+    if (not isHourAndMinutesValid(hours, minutes))
+    {
+        throw std::invalid_argument("Time Not Valid! Time Format (hours,minutes), minutes = [0,60)");
+    }
+    this->hours = hours;
+    this->minutes = minutes;
+}
+
+int Time::getHours() const
+{
+    return hours;
+}
+
+int Time::getMinutes() const
+{
+    return minutes;
+}
+
+bool Time::isTimeValid(const Time &time) const
+{
+    return isHourAndMinutesValid(time.getHours(), time.getMinutes());
+}
+
+bool Time::isHourAndMinutesValid(int hours, int minutes) const
+{
+    if (minutes < 0 or minutes >= 60)
+        return false;
+    return true;
+}
+
+Time Time::negate()
+{
+    this->hours = this->hours * -1;
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &out, const Time &time)
+{
+    auto paddingChar = '0';
+    auto paddingLength = 2;
+    out << "Time: " << time.getHours() << ":" << std::setfill(paddingChar) << std::setw(paddingLength) << time.getMinutes();
     return out;
+}
+
+bool Time::operator==(const Time &r_time) const
+{
+    return this->getHours() == r_time.getHours() and
+           this->getMinutes() == r_time.getMinutes();
+}
+
+bool Time::operator!=(const Time &r_time) const
+{
+    return this->getHours() != r_time.getHours() or
+           this->getMinutes() != r_time.getMinutes();
+}
+
+Time Time::operator+(const Time &r_time) const
+{
+    int calculatedHours = this->hours + r_time.hours;
+    int calculatedMinutes;
+
+    // FUNCTION:
+    if (r_time.getHours() >= 0)
+        calculatedMinutes = this->minutes + r_time.minutes;
+    else
+        calculatedMinutes = this->minutes - r_time.minutes;
+
+    if (calculatedMinutes >= 60)
+    {
+        calculatedHours += std::ceil(calculatedMinutes / 60);
+        auto minutesOverflow = calculatedMinutes % 60;
+        calculatedMinutes = minutesOverflow;
+    }
+    if (calculatedHours >= 24)
+    {
+        calculatedHours = calculatedHours % 24;
+    }
+    return Time(std::abs(calculatedHours), calculatedMinutes);
+}
+
+Time Time::operator-(const Time &r_time) const
+{
+    int calculatedHours;
+    if (std::abs(r_time.hours) > std::abs(this->hours))
+        calculatedHours = this->hours - std::abs(r_time.hours);
+    else
+        calculatedHours = this->hours - r_time.hours;
+    int calculatedMinutes = this->minutes - r_time.minutes;
+
+    if (calculatedHours < 0)
+    {
+        calculatedHours = 24 + calculatedHours;
+    }
+    if (calculatedMinutes < 0)
+    {
+        calculatedHours -= std::ceil(calculatedMinutes / 60) + 1;
+        auto minutesUnderflow = calculatedMinutes;
+        calculatedMinutes = 60 - std::abs(minutesUnderflow);
+    }
+    return Time(std::abs(calculatedHours), calculatedMinutes);
 }
